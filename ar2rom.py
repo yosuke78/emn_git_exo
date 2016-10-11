22314d25ad3a7ba6e5f2d36170ed9a1d072bc027
Christophe Augier <christophe.augier@gmail.com>
2016-10-11 19:06:53 +0200

    Again more tests
    Add more tests for converting values greater than 3.

--- ar2rom.py
+++ ar2rom.py
@@ -13,10 +13,27 @@ def ar2rom(val):
 class Tests(unittest.TestCase):
     def test_1_to_i(self):
         self.assertEqual(ar2rom(1), 'i')
+
     def test_2_to_ii(self):
         self.assertEqual(ar2rom(2), 'ii')
+
     def test_3_to_iii(self):
-        self.assertEqual(ar2rom(2), 'ii')
+        self.assertEqual(ar2rom(3), 'iii')
+
+    def test_4_to_iv(self):
+        self.assertEqual(ar2rom(4), 'iv')
+
+    def test_5_to_v(self):
+        self.assertEqual(ar2rom(5), 'v')
+
+    def test_10_to_x(self):
+        self.assertEqual(ar2rom(10), 'x')
+
+    def test_20_to_xx(self):
+        self.assertEqual(ar2rom(20), 'xx')
+
+    def test_21_to_xxi(self):
+        self.assertEqual(ar2rom(21), 'xxi')
 
 if __name__ == "__main__":
     unittest.main()
