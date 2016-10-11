d7f05fd696720c23c4d0df8909177f303dd983e0
Christophe Augier <christophe.augier@gmail.com>
2016-10-11 19:02:34 +0200

    Add more tests
    Test conversion of 2 and 3.

--- ar2rom.py
+++ ar2rom.py
@@ -8,6 +8,10 @@ def ar2rom(val):
 class Tests(unittest.TestCase):
     def test_1_to_i(self):
         self.assertEqual(ar2rom(1), 'i')
+    def test_2_to_ii(self):
+        self.assertEqual(ar2rom(2), 'ii')
+    def test_3_to_iii(self):
+        self.assertEqual(ar2rom(2), 'ii')
 
 if __name__ == "__main__":
     unittest.main()
