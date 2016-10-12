3d9a476783f9aad056eef53cd3592d5ff195c266
yosuke78 <vannasay.souvannavong@gmail.com>
2016-10-12 11:35:21 +0200

    add 50 in conversionmap

--- ar2rom.py
+++ ar2rom.py
@@ -5,7 +5,8 @@ conversion_map = (
     ('ix', 9),
     ('v', 5),
     ('iv', 4),
-    ('i', 1))
+    ('i', 1),
+    ('l', 50))
 
 
 def ar2rom(val):
@@ -44,6 +45,9 @@ class Tests(unittest.TestCase):
 
     def test_21_to_xxi(self):
         self.assertEqual(ar2rom(21), 'xxi')
+
+    def test_50_to_l(self):
+        self.assertEqual(ar2rom(50), 'l')
 
 if __name__ == "__main__":
     unittest.main()
