6fd33514a1a6e454224fd475f72473fdc59a7260
yosuke78 <vannasay.souvannavong@gmail.com>
2016-10-12 11:43:14 +0200

    capitalize roman numbers

--- ar2rom.py
+++ ar2rom.py
@@ -19,31 +19,31 @@ def ar2rom(val):
 
 class Tests(unittest.TestCase):
     def test_1_to_i(self):
-        self.assertEqual(ar2rom(1), 'i')
+        self.assertEqual(ar2rom(1), 'I')
 
     def test_2_to_ii(self):
-        self.assertEqual(ar2rom(2), 'ii')
+        self.assertEqual(ar2rom(2), 'II')
 
     def test_3_to_iii(self):
-        self.assertEqual(ar2rom(3), 'iii')
+        self.assertEqual(ar2rom(3), 'III')
 
     def test_4_to_iv(self):
-        self.assertEqual(ar2rom(4), 'iv')
+        self.assertEqual(ar2rom(4), 'IV')
 
     def test_5_to_v(self):
-        self.assertEqual(ar2rom(5), 'v')
+        self.assertEqual(ar2rom(5), 'V')
 
     def test_9_to_ix(self):
         self.assertEqual(ar2rom(9), 'ix')
 
     def test_10_to_x(self):
-        self.assertEqual(ar2rom(10), 'x')
+        self.assertEqual(ar2rom(10), 'X')
 
     def test_20_to_xx(self):
-        self.assertEqual(ar2rom(20), 'xx')
+        self.assertEqual(ar2rom(20), 'XX')
 
     def test_21_to_xxi(self):
-        self.assertEqual(ar2rom(21), 'xxi')
+        self.assertEqual(ar2rom(21), 'XXI')
 
 if __name__ == "__main__":
     unittest.main()
