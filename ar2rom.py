a43c537f3b1fb5de5df725e9ddc2a46cf0e78ef9
yosuke78 <vannasay.souvannavong@gmail.com>
2016-10-12 13:45:29 +0200

    capitalize letters

--- ar2rom.py
+++ ar2rom.py
@@ -11,29 +11,29 @@ def ar2rom(val):
 
 
 class Tests(unittest.TestCase):
-    def test_1_to_i(self):
-        self.assertEqual(ar2rom(1), 'i')
+    def test_1_to_I(self):
+        self.assertEqual(ar2rom(1), 'I')
 
-    def test_2_to_ii(self):
-        self.assertEqual(ar2rom(2), 'ii')
+    def test_2_to_II(self):
+        self.assertEqual(ar2rom(2), 'II')
 
-    def test_3_to_iii(self):
-        self.assertEqual(ar2rom(3), 'iii')
+    def test_3_to_III(self):
+        self.assertEqual(ar2rom(3), 'III')
 
-    def test_4_to_iv(self):
-        self.assertEqual(ar2rom(4), 'iv')
+    def test_4_to_IV(self):
+        self.assertEqual(ar2rom(4), 'IV')
 
-    def test_5_to_v(self):
-        self.assertEqual(ar2rom(5), 'v')
+    def test_5_to_V(self):
+        self.assertEqual(ar2rom(5), 'V')
 
-    def test_10_to_x(self):
-        self.assertEqual(ar2rom(10), 'x')
+    def test_10_to_X(self):
+        self.assertEqual(ar2rom(10), 'X')
 
-    def test_20_to_xx(self):
-        self.assertEqual(ar2rom(20), 'xx')
+    def test_20_to_XX(self):
+        self.assertEqual(ar2rom(20), 'XX')
 
-    def test_21_to_xxi(self):
-        self.assertEqual(ar2rom(21), 'xxi')
+    def test_21_to_XXI(self):
+        self.assertEqual(ar2rom(21), 'XXI')
 
 if __name__ == "__main__":
     unittest.main()
